b17b84bab655eebecb7df3d52bf39f68ea863d1d
Abdurashidbek Sadullayev <135204006+iosproger@users.noreply.github.com>
2024-12-19 14:04:39 +0500

    Update urls.py
    I add api for integration

--- src/expenses/urls.py
+++ src/expenses/urls.py
@@ -8,4 +8,4 @@ urlpatterns = [
     path('api/expenses/add', views.add_expense, name='add_expense'),
     path('delete/<int:id>', views.delete_expense, name='delete_expense'),
     path('edit/<int:id>', views.edit_expense, name='edit_expense'),  # New edit path
-]
+]  
